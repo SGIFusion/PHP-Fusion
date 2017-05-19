b8fd0ad89a3eeeedd6d57ba7149de2975b8ac07a
Septron <support@septron.de>
2017-05-19 11:11:15 +0200

    Update user_facebook_include_var.php

--- includes/user_fields/user_facebook_include_var.php
+++ includes/user_fields/user_facebook_include_var.php
@@ -20,7 +20,7 @@ if (!defined("IN_FUSION")) {
 }
 
 // Version of the user fields api
-$user_field_api_version = "1.01.00";
+$user_field_api_version = "1.02.00";
 $user_field_name = $locale['uf_facebook'];
 $user_field_desc = $locale['uf_facebook_desc'];
 $user_field_dbname = "user_facebook";
